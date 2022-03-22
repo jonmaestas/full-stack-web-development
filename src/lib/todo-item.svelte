cbe076d9dce657618383eb9ac3a9832951c9ea7a
Jon Maestas <jonathan.maestas@outlook.com>
2022-03-22 04:02:53 +0000

    added todo item; setup html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" name="" id="" />
+        <button aria-label="Save to do">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete to do">Delete</button>
+    </form>
+</div>
