18cfa33cac201234033430a212e281b7f12031a6
Jon Maestas <jonathan.maestas@outlook.com>
2022-03-31 03:53:51 +0000

    setup prisma integration

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -10,7 +10,6 @@ export const post: RequestHandler = async (request) => {
     const data = await request.request.formData();
 
     return api(request, {
-        uid: `${Date.now()}`, //TODO: update uuid generator
         created_at: new Date(),
         text: data.get("text").toString(),
         done: false,
